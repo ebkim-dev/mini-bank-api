bdad84da9ac072d105ed4d186f455be56bfdcd0a
onereward <onereward3118@gmail.com>
2026-02-11 14:30:28 -0500

    updates prismClient.ts

--- src/db/prismaClient.ts
+++ src/db/prismaClient.ts
@@ -13,11 +13,11 @@ import { PrismaMariaDb } from '@prisma/adapter-mariadb';
 // });
 
 const adapter = new PrismaMariaDb({
-  host: process.env.MYSQL_HOST!,
-  user: process.env.MYSQL_USER!,
-  password: process.env.MYSQL_PASSWORD!,
-  database: process.env.MYSQL_DB!,
-  port: Number(process.env.MYSQL_PORT)!,
+  host: process.env.MYSQL_HOST !,
+  user: process.env.MYSQL_USER !,
+  password: process.env.MYSQL_PASSWORD !,
+  database: process.env.MYSQL_DB !,
+  port: Number(process.env.MYSQL_PORT) !,
 
   // talk to mayank
   // --> cloudside config. ok to proceed or not?
