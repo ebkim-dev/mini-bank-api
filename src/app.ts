0da6ee8ab19b6748681f4e511b16988b68168322
Byunggyu.Kim <Byunggyu.Kim@fdmgroup.com>
2026-02-14 17:28:04 -0500

    integrate swagger, add docs for accounts domain

--- src/app.ts
+++ src/app.ts
@@ -6,8 +6,13 @@ import { requestLoggerMiddleware } from "./middleware/requestLogger";
 import { notFoundHandler, errorHandler } from "./middleware/errorHandler";
 import accountRouter from './routes/accountRouter';
 
+import swaggerUi from 'swagger-ui-express';
+import { swaggerSpec } from './config/swagger';
+
 export const createApp = (): Application => {
   const app = express();
+  
+  console.log("Swagger spec loaded?", swaggerSpec ? "YES" : "NO");
 
   app.use(express.json());
   app.use(traceIdMiddleware);
@@ -15,6 +20,7 @@ export const createApp = (): Application => {
 
   app.use("/health", healthRouter);
   app.use("/accounts", accountRouter);
+  app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
   app.use(notFoundHandler);
   app.use(errorHandler);
