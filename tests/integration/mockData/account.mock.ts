180c6c7f7c2b12d26efce75ed1596ba8af03ce7e
onereward <onereward3118@gmail.com>
2026-02-24 14:22:50 -0500

    Completed with accounts integration test.

--- tests/integration/mockData/account.mock.ts
+++ tests/integration/mockData/account.mock.ts
@@ -5,25 +5,48 @@ export type AccountCreateInputOptionals = {
   nickname?: string;
   status?: AccountStatus;
   balance?: string;
-}
+};
 
-export function buildAccountCreateInput(overrides = {}) {
+
+export type AccountCreateInput = {
+  customer_id: string;
+  type: AccountType;
+  currency: string;
+  nickname?: string;
+  status?: AccountStatus;
+  balance?: string;
+};
+
+export type AccountCreateOutput = {
+  customer_id: string;
+  type: AccountType;
+  currency: string;
+  nickname: string;
+  status: AccountStatus;
+  balance: string;
+};
+
+export function buildAccountCreateInput(
+  overrides: Partial<AccountCreateInput> = {}
+): AccountCreateInput {
   return {
     customer_id: "1",
     type: AccountType.SAVINGS,
     currency: "USD",
     ...overrides,
-  }
+  };
 }
 
-export function buildAccountCreateOutput(overrides = {}) {
+export function buildAccountCreateOutput(
+  overrides: Partial<AccountCreateOutput> = {}
+): AccountCreateOutput {
   return {
     customer_id: "1",
     type: AccountType.SAVINGS,
     currency: "USD",
     nickname: "alice",
     status: AccountStatus.ACTIVE,
-    balance: (new Decimal(0)).toString(),
+    balance: new Decimal(0).toString(),
     ...overrides,
-  }
+  };
 }
